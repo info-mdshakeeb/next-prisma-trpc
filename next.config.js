c93b69000aa32743b729170a51e90037b2c29449
m3-shakeeb <shakeeb.m360ict@gmail.com>
2025-11-12 11:57:41 +0600

    [added] : env type safety

---
+++ next.config.js
@@ -0,0 +1,21 @@
+import "@/env.js";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typedRoutes: true,
+  allowedDevOrigins: ["10.10.220.31"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
+  }
+};
+
+export default nextConfig;
---
+++ src/env.js
@@ -0,0 +1,39 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(["development", "test", "production"])
+      .default("development"),
+  },
+
+  /**
+   * Client-side env vars
+   * Must be prefixed with NEXT_PUBLIC_
+   */
+  client: {
+    // NEXT_PUBLIC_: z.string()
+  },
+
+  /**
+   * Map actual process.env vars
+   */
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+
+    // Client
+    // NEXT_PUBLIC_: process.env.NEXT_PUBLIC_,
+
+  },
+
+  /**
+   * Skip validation in Docker or CI builds
+*/
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+
+  /**
+   * Treat empty strings as undefined
+   */
+  emptyStringAsUndefined: true,
+});
