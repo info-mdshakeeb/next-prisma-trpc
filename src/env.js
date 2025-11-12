3a7c24237328d9f07678467d5a5637fd9fa40336
m3-shakeeb <shakeeb.m360ict@gmail.com>
2025-11-12 16:36:09 +0600

    [config] : better auth

--- src/env.js
+++ src/env.js
@@ -7,6 +7,8 @@ export const env = createEnv({
       .enum(["development", "test", "production"])
       .default("development"),
     DATABASE_URL: z.string(),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string()
   },
 
   /**
@@ -23,7 +25,8 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL: process.env.DATABASE_URL,
-
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
     // Client
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
 
