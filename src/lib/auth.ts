a7566c7595a51f500cdf843c5580c360ad66b469
itzshakeeb <itzshakeeb@gmail.com>
2025-12-01 07:13:43 +0600

    code update

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { nextCookies } from "better-auth/next-js";
+
 import prisma from "./db";
 
 export const auth = betterAuth({
--- src/proxy.ts
+++ src/proxy.ts
@@ -48,16 +48,3 @@ export async function proxy(request: NextRequest) {
 
   return NextResponse.next()
 }
-
-export const config = {
-  runtime: "nodejs",
-  matcher: [
-    "/dashboard/:path*",
-    "/settings/:path*",
-    "/profile/:path*",
-    "/users/:path*",
-    "/login",
-    "/register",
-    "/forgot-password",
-  ],
-};
